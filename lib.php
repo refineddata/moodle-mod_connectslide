907d25ba1a42e9adf16f16b655257ba16d90b125
Elvis <elvis.li@refineddata.com>
2018-02-15 11:17:25 -0500

    RT-241#Sherbrooke: error with the gear option in plugin

--- lib.php
+++ lib.php
@@ -1073,7 +1073,7 @@ function connectslide_create_display( $connectslide ){
         }
         $overtext .= $strtime . $strtele;
 
-        if (!empty($PAGE->context->id) && $PAGE->user_allowed_editing() && !empty($USER->editing)) {
+        if (!empty($PAGE->context->id) && $PAGE->user_allowed_editing() && !empty($USER->editing) && empty(strstr($PAGE->url, 'launch'))) {
             if( $course = $DB->get_record( 'course', array( 'id' => $connectslide->course ) ) ){
                 $editcontext = context_course::instance($course->id);
             }else{
